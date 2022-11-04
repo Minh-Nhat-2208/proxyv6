1141adaa683c3b29e360ce5859b80b2f6a6f465b
Minh-Nhat-2208 <117482323+Minh-Nhat-2208@users.noreply.github.com>
2022-11-05 04:00:24 +0700

    Update and rename run_code_v2.sh to run_code_v3.sh

--- run_code_v3.sh
+++ run_code_v3.sh
@@ -19,7 +19,7 @@ gen_data() {
 }
 
 IP4=$(curl -4 -s icanhazip.com)
-IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
+IP6=$(curl -6 -s icanhazip.com | cut -f1-3 -d':')
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
