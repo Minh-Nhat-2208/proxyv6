1d460bd7f7318b91acb8d25e332bacf327c8daba
Minh-Nhat-2208 <117482323+Minh-Nhat-2208@users.noreply.github.com>
2022-11-05 04:21:37 +0700

    Update installing_3proxy.sh

--- installing_3proxy.sh
+++ installing_3proxy.sh
@@ -1,8 +1,12 @@
+echo "installing apps"
+yum -y install gcc net-tools bsdtar zip >/dev/null
+
 install_3proxy() {
     echo "installing 3proxy"
-    URL="https://raw.githubusercontent.com/quayvlog/quayvlog/main/3proxy-3proxy-0.8.6.tar.gz"
+    # URL="https://raw.githubusercontent.com/quayvlog/quayvlog/main/3proxy-3proxy-0.8.6.tar.gz"
+    URL="https://raw.githubusercontent.com/Minh-Nhat-2208/proxyv6/main/3proxy-master.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-3proxy-0.8.6
+    cd 3proxy-3proxy-0.9.3
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
     cp src/3proxy /usr/local/etc/3proxy/bin/
