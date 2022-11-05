b5b5da2082296359172a79d7327b61de122d1459
Minh-Nhat-2208 <117482323+Minh-Nhat-2208@users.noreply.github.com>
2022-11-05 14:41:14 +0700

    Update TRX_2.sh

--- TRX_2.sh
+++ TRX_2.sh
@@ -16,11 +16,11 @@ install_3proxy() {
     #URL="https://raw.githubusercontent.com/quayvlog/quayvlog/main/3proxy-3proxy-0.8.6.tar.gz"
     #wget -qO- $URL | bsdtar -xvf-
     #cd 3proxy-3proxy-0.8.6
-    make -f Makefile.Linux
-    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
-    cp src/3proxy /usr/local/etc/3proxy/bin/
-    cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
-    chmod +x /etc/init.d/3proxy
+    #make -f Makefile.Linux
+    #mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
+    #cp src/3proxy /usr/local/etc/3proxy/bin/
+    #cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
+    #chmod +x /etc/init.d/3proxy
     chkconfig 3proxy on
     cd $WORKDIR
 }
